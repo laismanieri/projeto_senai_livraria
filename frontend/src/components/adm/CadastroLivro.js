8ba683b3a229166dd2079f2f70b54a963d3bcb59
laismanieri <lais.manieri@hotmail.com>
2023-05-20 23:12:13 -0300

    inclusão lista de generos

--- frontend/src/components/adm/CadastroLivro.js
+++ frontend/src/components/adm/CadastroLivro.js
@@ -4,70 +4,70 @@ import axios from "axios";
 import styles from "../adm/CadastroLivro.module.css";
 
 function CadastroLivro() {
-const [isExpanded, setIsExpanded] = useState(false);
-const [tipoLivroAdd, setTipoLivroAdd] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [tipoLivroAdd, setTipoLivroAdd] = useState(false);
 
-const [titulo, setTitulo] = useState("");
-const [anoPublicacao, setAnoPublicacao] = useState("");
-const [autor, setAutor] = useState("");
-const [sinopse, setSinopse] = useState("");
-const [genero, setGenero] = useState("");
-const [editora, setEditora] = useState("");
-const [qtdePagina, setQtdePagina] = useState("");
-const [oferta, setOferta] = useState("");
-const [destaque, setDestaque] = useState("");
-const [imagem, setImagem] = useState("");
-const [tipoLivro, setTipoLivro] = useState("");
-const [preco, setPreco] = useState("");
-const [qtdeEstoque, setQtdeEstoque] = useState("");
-const [detalhes, setDetalhes] = useState([]);
+  const [titulo, setTitulo] = useState("");
+  const [anoPublicacao, setAnoPublicacao] = useState("");
+  const [autor, setAutor] = useState("");
+  const [sinopse, setSinopse] = useState("");
+  const [genero, setGenero] = useState("");
+  const [editora, setEditora] = useState("");
+  const [qtdePagina, setQtdePagina] = useState("");
+  const [oferta, setOferta] = useState("");
+  const [destaque, setDestaque] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [tipoLivro, setTipoLivro] = useState("");
+  const [preco, setPreco] = useState("");
+  const [qtdeEstoque, setQtdeEstoque] = useState("");
+  const [detalhes, setDetalhes] = useState([]);
 
-const tipoLivroFisico = tipoLivro === "FISICO";
-const tipoLivroEbook = tipoLivro === "EBOOK";
+  const tipoLivroFisico = tipoLivro === "FISICO";
+  const tipoLivroEbook = tipoLivro === "EBOOK";
 
-const handleSubmit = (e) => {
-  e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const novoLivro = {
-    titulo: titulo,
-    autor: autor,
-    anoPublicacao: anoPublicacao,
-    sinopse: sinopse,
-    genero: genero,
-    editora: editora,
-    qtdePagina: qtdePagina,
-    oferta: oferta,
-    destaque: destaque,
-    imagem: imagem,
-    detalhes: detalhes, 
+    const novoLivro = {
+      titulo: titulo,
+      autor: autor,
+      anoPublicacao: anoPublicacao,
+      sinopse: sinopse,
+      genero: genero,
+      editora: editora,
+      qtdePagina: qtdePagina,
+      oferta: oferta,
+      destaque: destaque,
+      imagem: imagem,
+      detalhes: detalhes,
+    };
+
+    axios
+      .post("http://localhost:8082/livro", novoLivro)
+      .then((response) => {
+        console.log("Livro cadastrado com sucesso:", response.data);
+        // Resetar os campos do formulário
+        setTitulo("");
+        setAutor("");
+        setAnoPublicacao("");
+        setSinopse("");
+        setGenero("");
+        setEditora("");
+        setQtdePagina("");
+        setOferta("");
+        setDestaque("");
+        setDetalhes([]);
+        setTipoLivro("");
+        setPreco("");
+        setQtdeEstoque("");
+
+        window.alert("Livro cadastrado!");
+      })
+      .catch((error) => {
+        console.error("Erro ao cadastrar livro:", error);
+        window.alert("Erro ao cadastrar livro!");
+      });
   };
-
-  axios
-    .post("http://localhost:8082/livro", novoLivro)
-    .then((response) => {
-      console.log("Livro cadastrado com sucesso:", response.data);
-      // Resetar os campos do formulário
-      setTitulo("");
-      setAutor("");
-      setAnoPublicacao("");
-      setSinopse("");
-      setGenero("");
-      setEditora("");
-      setQtdePagina("");
-      setOferta("");
-      setDestaque("");
-      setDetalhes([]);
-      setTipoLivro("");
-      setPreco("");        
-      setQtdeEstoque("");
-
-      window.alert("Livro cadastrado!");
-    })
-    .catch((error) => {
-      console.error("Erro ao cadastrar livro:", error);
-      window.alert("Erro ao cadastrar livro!");
-    });
-};
 
   const handleToggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -128,18 +128,49 @@ const handleSubmit = (e) => {
               <div className={styles.containerSmall}>
                 <Row>
                   <Col>
-                    <Form.Group controlId="formGenero">
-                      <Form.Label className={styles.labelCadastro}>
+                    <Form.Group controlId="formGenero" as={Row}>
+                      <Form.Label
+                        column
+                        sm="12"
+                        className={styles.labelCadastro}
+                      >
                         Gênero
                       </Form.Label>
-                      <Form.Control
-                        className={styles.inputCadastrar}
-                        type="text"
-                        rows={3}
-                        placeholder="Digite o genero do livro"
-                        value={genero}
-                        onChange={(e) => setGenero(e.target.value)}
-                      />
+                      <Col sm="12">
+                        <Form.Control
+                          className={styles.inputCadastrar}
+                          as="select"
+                          value={genero}
+                          onChange={(e) => setGenero(e.target.value)}
+                        >
+                          <option value="">Selecione um gênero</option>
+                          <option value="Ação">Ação</option>
+                          <option value="Aventura">Aventura</option>
+                          <option value="Romance">Romance</option>
+                          <option value="Ficção Científica">
+                            Ficção Científica
+                          </option>
+                          <option value="Fantasia">Fantasia</option>
+                          <option value="Suspense">Suspense</option>
+                          <option value="Mistério">Mistério</option>
+                          <option value="Horror">Horror</option>
+                          <option value="Drama">Drama</option>
+                          <option value="Comédia">Comédia</option>
+                          <option value="Biografia">Biografia</option>
+                          <option value="Autobiografia">Autobiografia</option>
+                          <option value="História">História</option>
+                          <option value="Autoajuda">Autoajuda</option>
+                          <option value="Negócios">Negócios</option>
+                          <option value="Autoconhecimento">
+                            Autoconhecimento
+                          </option>
+                          <option value="Autores Nacionais">
+                            Autores Nacionais
+                          </option>
+                          <option value="Poesia">Poesia</option>
+                          <option value="Poesia">Outros</option>
+                        </Form.Control>
+                      </Col>
                     </Form.Group>
                   </Col>
                 </Row>
@@ -290,8 +321,8 @@ const handleSubmit = (e) => {
               </Row>
 
               <div className={styles.containerTtipoLivro}>
-                <div >
-                <Row>
+                <div>
+                  <Row>
                     <Col>
                       <Form.Group controlId="formTipoLivro" as={Row}>
                         <Form.Label
@@ -352,7 +383,7 @@ const handleSubmit = (e) => {
                   </Row>
                 </div>
 
-                <div >
+                <div>
                   <Row>
                     <Col>
                       <Form.Group controlId="formTipoLivro" as={Row}>
