238c7d4d3b9de6740096c4e9ca9ddc4dcd64eefa
laismanieri <lais.manieri@hotmail.com>
2023-05-23 14:12:24 -0300

    persistindo o usuario logado

--- frontend/src/components/layout/AuthContext.js
+++ frontend/src/components/layout/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 export const AuthContext = createContext();
@@ -6,10 +6,14 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  // Função para fazer o login do usuário
   const login = async (email, senha) => {
     try {
       const response = await axios.post("http://localhost:8082/usuario/login", { email, senha });
       const userData = response.data;
+
+      // Armazena os dados do usuário no localStorage
+      localStorage.setItem("userData", JSON.stringify(userData));
 
       setUser(userData);
     } catch (error) {
@@ -18,9 +22,23 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  // Função para fazer o logout do usuário
   const logout = () => {
+    // Remove os dados do usuário do localStorage
+    localStorage.removeItem("userData");
+
     setUser(null);
   };
+
+  // Verifica se existem dados do usuário armazenados no localStorage ao carregar o componente
+  useEffect(() => {
+    const storedUserData = localStorage.getItem("userData");
+
+    if (storedUserData) {
+      const userData = JSON.parse(storedUserData);
+      setUser(userData);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
